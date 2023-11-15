8700a9f4580daf498d13251cea97e85fd58cc83e
zhouqiang11111 <131637400+zhouqiang11111@users.noreply.github.com>
2023-11-15 11:12:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-// app.js (可以为空，这里是为了展示可能的 JavaScript 文件)
+// app.js
 document.addEventListener('DOMContentLoaded', function() {
-    // 添加你的 JavaScript 代码
-    console.log('My Awesome Project is loaded!');
+    createPetals();
 });
+
+function createPetals() {
+    const numPetals = 50;
+
+    for (let i = 0; i < numPetals; i++) {
+        const petal = document.createElement('div');
+        petal.className = 'petal';
+        petal.style.left = `${Math.random() * 100}vw`;
+        petal.style.background = getRandomColor();
+        petal.style.animationDuration = `${Math.random() * 2 + 1}s`;
+        document.body.appendChild(petal);
+    }
+}
+
+function getRandomColor() {
+    const colors = ['#ff6666', '#ffcc66', '#66ff66', '#6666ff', '#cc66ff'];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
