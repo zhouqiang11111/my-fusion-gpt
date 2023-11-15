5edf4ff5b5c07a889a379136951baeaede93283f
zhouqiang11111 <131637400+zhouqiang11111@users.noreply.github.com>
2023-11-15 10:38:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// app.js 
+// app.js (可以为空，这里是为了展示可能的 JavaScript 文件)
+document.addEventListener('DOMContentLoaded', function() {
+    // 添加你的 JavaScript 代码
+    console.log('My Awesome Project is loaded!');
+});
